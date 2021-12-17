78e9396445720efb4e3488e8365ebecb4d2744d2
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2021-12-17 11:51:32 +0300

    Refactored Transaction entity

--- src/main/java/com/dzvonik/cashflow/domain/entity/Transaction.java
+++ src/main/java/com/dzvonik/cashflow/domain/entity/Transaction.java
@@ -3,7 +3,7 @@ package com.dzvonik.cashflow.domain.entity;
 import com.dzvonik.cashflow.domain.entity.enums.TransactionType;
 
 import java.math.BigDecimal;
-import java.util.List;
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,12 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Enumerated;
 import javax.persistence.EnumType;
-import javax.persistence.CascadeType;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
 
 import lombok.Getter;
@@ -34,14 +30,6 @@ public class Transaction {
     @EqualsAndHashCode.Include
     private int id;
 
-    @ManyToMany(cascade = CascadeType.PERSIST)
-    @JoinTable(
-            name = "TRANSACTION_CATEGORY",
-            joinColumns = @JoinColumn(name = "transaction_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id")
-    )
-    private List<Category> categories;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id")
     private Account account;
@@ -50,6 +38,10 @@ public class Transaction {
 
     @Enumerated(EnumType.STRING)
     private TransactionType type;
+
+    private Integer categoryId;
+
+    private LocalDate date;
 
     private String comment;
 
