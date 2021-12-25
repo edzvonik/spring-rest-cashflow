83458eef5021752fe75dd2943580997f6ed9c0e6
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2021-12-25 13:40:40 +0300

    Refactored changelogs
    Changes:
    - names of changelogs
    - IDs for every changeset
    - deleted tables: account-transactions, user-accounts

--- src/main/resources/db/changelog/changes/2021-12-22__init-db.sql
+++ src/main/resources/db/changelog/changes/2021-12-22__init-db.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset edzvonik:id1
+--changeset edzvonik:1
 create sequence seq_account start 1 increment 100;
 
 create sequence seq_category start 1 increment 1000;
@@ -18,16 +18,10 @@ create table account
     primary key (id)
 );
 
-create table account_categories
+create table account_category
 (
     account_id    int4 not null,
     categories_id int4 not null
-);
-
-create table account_transactions
-(
-    account_id      int4 not null,
-    transactions_id int4 not null
 );
 
 create table category
@@ -37,7 +31,7 @@ create table category
     primary key (id)
 );
 
-create table category_transactions
+create table category_transaction
 (
     category_id     int4 not null,
     transactions_id int4 not null
@@ -64,49 +58,25 @@ create table "user"
     primary key (id)
 );
 
-create table user_accounts
-(
-    user_id     varchar(255) not null,
-    accounts_id int4         not null
-);
-
-alter table if exists account_transactions
-    add constraint uc_account_transactions unique (transactions_id);
-
-alter table if exists category_transactions
+alter table if exists category_transaction
     add constraint uc_category_transactions unique (transactions_id);
 
-alter table if exists user_accounts
-    add constraint uc_user_accounts unique (accounts_id);
-
-alter table if exists account_categories
+alter table if exists account_category
     add constraint fk_account_categories_category
     foreign key (categories_id) references category;
 
-alter table if exists account_categories
+alter table if exists account_category
     add constraint fk_account_categories_account
     foreign key (account_id) references account;
 
-alter table if exists account_transactions
+alter table if exists account_transaction
     add constraint fk_account_transactions_transaction
     foreign key (transactions_id) references transaction;
 
-alter table if exists account_transactions
-    add constraint fk_account_transactions_account
-    foreign key (account_id) references account;
-
-alter table if exists category_transactions
+alter table if exists category_transaction
     add constraint fk_category_transactions_transaction
     foreign key (transactions_id) references transaction;
 
-alter table if exists category_transactions
+alter table if exists category_transaction
     add constraint fk_category_transactions_category
     foreign key (category_id) references category;
-
-alter table if exists user_accounts
-    add constraint fk_user_accounts_account
-    foreign key (accounts_id) references account;
-
-alter table if exists user_accounts
-    add constraint fk_user_accounts_user
-    foreign key (user_id) references "user";
