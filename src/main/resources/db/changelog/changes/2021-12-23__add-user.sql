b706533fc7b4fbeddc44a17749f965653206e764
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2022-01-17 14:56:09 +0300

    Add changelog 2022-01-17.sql which add data to database

--- src/main/resources/db/changelog/changes/2021-12-23__add-user.sql
+++ src/main/resources/db/changelog/changes/2021-12-23__add-user.sql
@@ -1,6 +1,59 @@
 --liquibase formatted sql
 
 --changeset edzvonik:2
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
 
-insert into "user"(id, name, email, base_currency) values (1, 'Petr', 'petr@user.com', 'USD');
-select setval('seq_user', 1);
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
+
+insert into "user"(id, name, email, base_currency) values (nextval('seq_user'), 'Petr', 'user@user.com', 'USD');
