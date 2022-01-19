08655a7be9ad211c4f37e6056809ccba36811374
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2022-01-19 10:15:01 +0300

    -

---
+++ src/main/java/com/dzvonik/cashflow/domain/entity/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dzvonik.cashflow.domain.entity.repository;
+
+import com.dzvonik.cashflow.domain.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {}
+
